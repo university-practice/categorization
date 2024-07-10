b674419ec423e161938970b8fe23af1acf7aa8f0
Denis Beresnev <shanthi.bunch@gmail.com>
2024-07-10 10:46:30 +0300

    feat: backend template

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
+def hello():
+   return {"message": "Hello, World!"}
+
+@app.post("//api/categorize")
 def read_root():
+	# read body
+
+	# read file
+
+	# http request bert:80801/predict
+	# response1 0.8
+
+	# http request tf-idf:80801/predict
+	# response2 0.7
+
+	# merge results
+
+	# work with file
+
+	# return file
     return {"message": "Hello, World!"}
