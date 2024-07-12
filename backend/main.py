2d6e667d4df6a35aa3eba0da9fbfaecd8fce5685
Denis Beresnev <shanthi.bunch@gmail.com>
2024-07-12 14:55:18 +0300

    feat: added topics, refactor

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, HTTPException, UploadFile, File, Form
 import requests
 import pandas as pd
@@ -9,42 +10,43 @@ app = FastAPI()
 
 
 @app.post("/api/categorize")
-async def read_root(columnName: str = Form(), xlsxFile: UploadFile = File(...)):
-
-    print("custom log")
-
-    response1_data = None
-    response2_data = None
-
-    try:
-        response1 = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-        response1.raise_for_status()
-        response1_data = response1.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error with first fake request: {e}")
-
-    try:
-        response2 = requests.get("https://jsonplaceholder.typicode.com/posts/2")
-        response2.raise_for_status()
-        response2_data = response2.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error with second fake request: {e}")
-
-    if response1_data: print("Response 1:", response1_data)
-    if response2_data: print("Response 2:", response2_data)
-
-	# merge results
-
+async def read_root(
+	columnName: str = Form(),
+	topics: List[str] = Form(...),
+	xlsxFile: UploadFile = File(...)
+):
+    print("topics", topics)
     try:
         file_content = await xlsxFile.read()
         df = pd.read_excel(BytesIO(file_content))
-        df['prediction'] = 'prediction'
         output = BytesIO()
+
+        data = df.dropna(subset=[columnName])
+        print("TEST", data['Направление работы конференции'].unique().tolist())
+        for index, row in data.iterrows():
+            # response1_data = None
+            # response2_data = None
+
+            # try:
+            #     response1 = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+            #     response1.raise_for_status()
+            #     response1_data = response1.json()
+            # except requests.exceptions.RequestException as e:
+            #     print(f"Error with first fake request: {e}")
+
+            # try:
+            #     response2 = requests.get("https://jsonplaceholder.typicode.com/posts/2")
+            #     response2.raise_for_status()
+            #     response2_data = response2.json()
+            # except requests.exceptions.RequestException as e:
+            #     print(f"Error with second fake request: {e}")
+
+            data.at[index, 'prediction'] = row[columnName]
 
         # Запишите DataFrame обратно в BytesIO буфер
         output = BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-            df.to_excel(writer, index=False, sheet_name='Sheet1')
+            data.to_excel(writer, index=False, sheet_name='Sheet1')
 
         output.seek(0)
     except Exception as e:
