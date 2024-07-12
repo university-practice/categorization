fe60cb02ebe2ebed2c9d095b34b76e3d3cff526a
Denis Beresnev <shanthi.bunch@gmail.com>
2024-07-12 14:04:02 +0300

    feat: refactor and file response

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,59 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, HTTPException, UploadFile, File, Form
 import requests
 import pandas as pd
-from io import StringIO
+from io import BytesIO, StringIO
+
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
 
 @app.post("/api/categorize")
-async def read_root(columnName: str = Form(), csvFile: UploadFile = File(...)):
+async def read_root(columnName: str = Form(), xlsxFile: UploadFile = File(...)):
 
-	# http request bert:80801/predict
-	# response1 0.8
+    print("custom log")
 
-    response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+    response1_data = None
+    response2_data = None
 
-    if response.status_code == 200:
-        print(response.content)
-    elif response.status_code == 404:
-        print('Not Found.')
+    try:
+        response1 = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+        response1.raise_for_status()
+        response1_data = response1.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error with first fake request: {e}")
 
-	# http request tf-idf:80801/predict
-	# response2 0.7
+    try:
+        response2 = requests.get("https://jsonplaceholder.typicode.com/posts/2")
+        response2.raise_for_status()
+        response2_data = response2.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error with second fake request: {e}")
+
+    if response1_data: print("Response 1:", response1_data)
+    if response2_data: print("Response 2:", response2_data)
 
 	# merge results
 
-	# work with file
-    df = pd.read_csv(StringIO((await csvFile.read()).decode()))
-    csvFile.file.close()
-    df['prediction'] = 'prediction'
+    try:
+        file_content = await xlsxFile.read()
+        df = pd.read_excel(BytesIO(file_content))
+        df['prediction'] = 'prediction'
+        output = BytesIO()
 
-    # return file
-    return df.to_csv(index=False)
+        # Запишите DataFrame обратно в BytesIO буфер
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, sheet_name='Sheet1')
+
+        output.seek(0)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Error processing xlsx file") from e
+    finally:
+        xlsxFile.file.close()
+
+    return StreamingResponse(
+        output,
+        media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        headers={"Content-Disposition": "attachment; filename=processed_file.xlsx"}
+    )
