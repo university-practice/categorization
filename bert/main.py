944c5dbd617ba9fa664dd44b1f2748c919a6dc55
Denis Beresnev <shanthi.bunch@gmail.com>
2024-07-15 04:19:03 +0300

    feat: bert template

--- bert/main.py
+++ bert/main.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import torch
+from transformers import DistilBertTokenizer, DistilBertModel, DistilBertConfig
+
 
 app = FastAPI()
 
+class TextInput(BaseModel):
+    text: str
 
-@app.post("/bert")
-async def read_root(request: dict):
+# Загрузка модели и токенайзера
+model_path = "./model/model_v0_1.pt"
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 
-    print(request)
+# Создание конфигурации модели
+config = DistilBertConfig.from_pretrained('distilbert-base-uncased')
 
-    return "hello"
+# Создание экземпляра модели
+model = DistilBertModel(config)
+
+# Загрузка весов модели с указанием map_location
+model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
 
+@app.post("/predict")
+async def predict(input: TextInput):
+    inputs = tokenizer(input.text, return_tensors="pt")
+    outputs = model(**inputs)
 
+    return outputs
